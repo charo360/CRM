f60dabdb59546c171145b796738bf67e004f4220
charo360 <sm1761a@american.edu>
2026-03-25 16:13:50 -0400

    fix: bump versionCode to 4 for Play Store resubmission

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -17,7 +17,7 @@ export default {
     },
     android: {
       package: "com.zilo.reply",
-      versionCode: 3,
+      versionCode: 4,
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#2DB843"
